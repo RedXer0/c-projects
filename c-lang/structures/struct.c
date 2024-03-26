f3fca9cf3c27e086ffe771e65e06fd6e4f448fa2
Red Xer0 <165036246+RedXer0@users.noreply.github.com>
2024-03-26 08:49:18 -0500

    Add files via upload

--- c-lang/structures/struct.c
+++ c-lang/structures/struct.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+
+void thestructure();
 struct mystructure {
     int age;
     float favnum;
+    char yourname[30];
 };
 
 int main(){
@@ -13,10 +16,20 @@ int main(){
     s1.favnum;
     printf("enter your favorite number:");
     scanf("%f", &s1.favnum);
+    s1.yourname;
+    printf("enter your name:");
+    scanf("%s", s1.yourname);
+
 
     printf("your age: %d\n", s1.age);
     printf("your favorite number: %.2f\n", s1.favnum);
+    printf("your name: %s\n", s1.yourname);
+    thestructure();
 
     return 0;
+}
 
+void thestructure(){
+    struct mystructure s1 = {33, 77.7, "Bobby"};
+    printf("your age:%d\nyour favorite number:%.2f\nyour name:%s\n",s1.age, s1.favnum, s1.yourname);
 }
